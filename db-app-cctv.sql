e93cbf02ed92c50b8a07745a3ec6a4c0517c8e7b
Muhammad Arya Danuarta <72311684+danuartax64@users.noreply.github.com>
2023-01-03 13:19:48 +0700

    Latest Dataset

--- db-app-cctv.sql
+++ db-app-cctv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 29 Des 2022 pada 05.51
+-- Waktu pembuatan: 03 Jan 2023 pada 07.18
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.0.25
 
@@ -31,16 +31,18 @@ CREATE TABLE `accounts` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `fullakses` char(1) DEFAULT NULL
+  `lokasi` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data untuk tabel `accounts`
 --
 
-INSERT INTO `accounts` (`id`, `username`, `password`, `fullakses`) VALUES
-(2, 'admin', 'cctv', '1'),
-(3, 'user', 'user', '0');
+INSERT INTO `accounts` (`id`, `username`, `password`, `lokasi`) VALUES
+(2, 'admin', 'cctv', 'ADMIN'),
+(3, 'user', 'user', 'PUBLIC'),
+(10, 'user2', 'user2', 'ICT'),
+(11, 'userhc', 'userhc', 'HC');
 
 -- --------------------------------------------------------
 
@@ -51,39 +53,39 @@ INSERT INTO `accounts` (`id`, `username`, `password`, `fullakses`) VALUES
 CREATE TABLE `list_cctv` (
   `nama_cctv` varchar(35) NOT NULL COMMENT 'nama cctv',
   `link` varchar(255) NOT NULL COMMENT 'link rtsp cctv',
-  `id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `lokasi` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data untuk tabel `list_cctv`
 --
 
-INSERT INTO `list_cctv` (`nama_cctv`, `link`, `id`) VALUES
-('HIKVISION HC', 'rtsp://admin:admin123@10.203.21.20:554/Streaming/Channels/102/', 10),
-('DC 1', 'rtsp://service:Az123456b$@10.203.2.56/?inst=2', 12),
-('DC 2', 'rtsp://service:Az123456b$@10.203.2.57/?inst=2', 13),
-('BOSCH Lorong Masuk ICT', 'rtsp://service:Az123456b$@10.203.2.64:554/?inst=2', 14),
-('Placeholder 10', 'https://63.142.183.154/mjpg/video.mjpg', 32),
-('DC Entrance', 'rtsp://service:Az123456b$@10.203.2.58/?inst=2', 45),
-('DC CP', 'rtsp://service:Az123456b$@10.203.2.59/?inst=2', 46),
-('Ruang Kerja ICT', 'rtsp://service:Az123456b$@10.203.2.60/?inst=2', 49),
-('NOC', 'rtsp://service:Az123456b$@10.203.2.61/?inst=2', 50),
-('DC 3 IR', 'rtsp://service:Az123456b$@10.203.2.62/?inst=2', 51),
-('DC UPS', 'rtsp://service:Az123456b$@10.203.2.63/?inst=2', 52),
-('DC UPS 2', 'rtsp://service:Az123456b$@10.203.2.65/?inst=2', 54),
-('DC Old', 'rtsp://service:Az123456b$@10.203.2.66/?inst=2', 55),
-('Hikvision HC 2', 'rtsp://admin:admin123@10.203.21.21:554/Streaming/Channels/102/', 56),
-('DC UPS 3', '	rtsp://service:Az123456b$@10.203.2.67/?inst=2', 58),
-('DC Old entrance', '	rtsp://service:Az123456b$@10.203.2.68/?inst=2', 59),
-('ICT Kitchen', '	rtsp://service:Az123456b$@10.203.2.69/?inst=2', 60),
-('Front Door ICT', '	rtsp://service:Az123456b$@10.203.2.70/?inst=2', 61),
-('DC UPS 5', '	rtsp://service:Az123456b$@10.203.2.71/?inst=2', 62),
-('Hikvision Finance', 'rtsp://admin:admin123@10.203.21.30:554/Streaming/Channels/102/', 63),
-('YPS', 'rtsp://admin:admin123@10.203.21.31:554/Streaming/Channels/102/', 64),
-('HIkvision jalan', 'rtsp://admin:admin123@10.203.21.34:554/Streaming/Channels/102/', 65),
-('indomaret', 'rtsp://admin:admin123@10.203.21.36:554/Streaming/Channels/102/', 66),
-('pos rs', 'rtsp://admin:admin123@10.203.21.40:554/Streaming/Channels/102/', 67),
-('test 42', 'rtsp://admin:admin123@10.203.21.42:554/Streaming/Channels/102/', 68);
+INSERT INTO `list_cctv` (`nama_cctv`, `link`, `id`, `lokasi`) VALUES
+('HIKVISION HC', 'rtsp://admin:admin123@10.203.21.20:554/Streaming/Channels/102/', 10, 'HC'),
+('DC 1', 'rtsp://service:Az123456b$@10.203.2.56/?inst=2', 12, 'ICT'),
+('DC 2', 'rtsp://service:Az123456b$@10.203.2.57/?inst=2', 13, 'ICT'),
+('BOSCH Lorong Masuk ICT', 'rtsp://service:Az123456b$@10.203.2.64:554/?inst=2', 14, 'ICT'),
+('DC Entrance', 'rtsp://service:Az123456b$@10.203.2.58/?inst=2', 45, 'ICT'),
+('DC CP', 'rtsp://service:Az123456b$@10.203.2.59/?inst=2', 46, 'ICT'),
+('Ruang Kerja ICT', 'rtsp://service:Az123456b$@10.203.2.60/?inst=2', 49, 'ICT'),
+('NOC', 'rtsp://service:Az123456b$@10.203.2.61/?inst=2', 50, 'ICT'),
+('DC 3 IR', 'rtsp://service:Az123456b$@10.203.2.62/?inst=2', 51, 'ICT'),
+('DC UPS', 'rtsp://service:Az123456b$@10.203.2.63/?inst=2', 52, 'ICT'),
+('DC UPS 2', 'rtsp://service:Az123456b$@10.203.2.65/?inst=2', 54, 'ICT'),
+('DC Old', 'rtsp://service:Az123456b$@10.203.2.66/?inst=2', 55, 'ICT'),
+('Hikvision HC 2', 'rtsp://admin:admin123@10.203.21.21:554/Streaming/Channels/102/', 56, 'HC'),
+('DC UPS 3', 'rtsp://service:Az123456b$@10.203.2.67/?inst=2', 58, 'ICT'),
+('DC Old entrance', 'rtsp://service:Az123456b$@10.203.2.68/?inst=2', 59, 'ICT'),
+('ICT Kitchen', 'rtsp://service:Az123456b$@10.203.2.69/?inst=2', 60, 'ICT'),
+('Front Door ICT', 'rtsp://service:Az123456b$@10.203.2.70/?inst=2', 61, 'ICT'),
+('DC UPS 5', 'rtsp://service:Az123456b$@10.203.2.71/?inst=2', 62, 'ICT'),
+('Hikvision Finance', 'rtsp://admin:admin123@10.203.21.30:554/Streaming/Channels/102/', 63, 'PUBLIC'),
+('YPS', 'rtsp://admin:admin123@10.203.21.31:554/Streaming/Channels/102/', 64, 'PUBLIC'),
+('HIkvision jalan', 'rtsp://admin:admin123@10.203.21.34:554/Streaming/Channels/102/', 65, 'PUBLIC'),
+('indomaret', 'rtsp://admin:admin123@10.203.21.36:554/Streaming/Channels/102/', 66, 'PUBLIC'),
+('pos rs', 'rtsp://admin:admin123@10.203.21.40:554/Streaming/Channels/102/', 67, 'PUBLIC'),
+('test 42', 'rtsp://admin:admin123@10.203.21.42:554/Streaming/Channels/102/', 68, 'PUBLIC');
 
 --
 -- Indexes for dumped tables
@@ -109,13 +111,13 @@ ALTER TABLE `list_cctv`
 -- AUTO_INCREMENT untuk tabel `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT untuk tabel `list_cctv`
 --
 ALTER TABLE `list_cctv`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=121;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
